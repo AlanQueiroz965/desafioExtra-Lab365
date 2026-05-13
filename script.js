da64d0c776d0069c1cdcc97e42c424b0a56a31ca
alancosta <iamforgiven70x70@gmail.com>
2026-05-12 21:57:59 -0300

    adição de css, javascript e html

---
+++ script.js
@@ -0,0 +1,17 @@
+const formElemento = document.querySelector("form");
+const inputsElementos = document.querySelectorAll("input");
+const textAreaElemento = document.querySelector("textarea");
+
+formElemento.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let valores = [];
+
+    inputsElementos.forEach(function (input) {
+        valores.push(input.value);
+
+        // console.log(input.value);
+    });
+    valores.push(textAreaElemento.value);
+
+});
